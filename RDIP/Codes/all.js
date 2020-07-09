de562720b9d69a10d18f988f69824f88d1f45070
HemangiSA <43313841+HemangiSA@users.noreply.github.com>
2020-07-09 18:15:48 +0530

    Goal 2 Corpus Requirements

---
+++ RDIP/Codes/all.js
@@ -0,0 +1,328 @@
+	var one="John ate an apple before afternoon";
+	var ones=one.split(' ');
+	document.write(ones);
+	
+	var alter1=["John ate an apple before afternoon"];
+	var alter2=["before afternoon John ate an apple"];
+	var alter3=[ "John before afternoon ate an apple"];
+   
+	
+	if(one==alter1 || one==alter2 || one==alter3)
+	{
+		alert("Right Answer");
+	}
+	else
+	{
+		alert("Wrong Answer");
+	}
+	
+	function func1()
+	{
+		document.write("<h1>Correct Answer</h1>");
+		document.write("<br>")
+		document.write("John ate an apple before afternoon");
+		document.write("<br>");
+		document.write("before afternoon John ate an apple"); 
+		document.write("<br>");
+		document.write("John before afternoon ate an apple");
+	}
+	
+	sleep(4000);
+	
+	var two="some students like to study in the night";
+	var twos=one.split(' ');
+	document.write(twos);
+	
+	var alter4=["some students like to study in the night"];
+	var alter5=["at night some students like to study"];
+   
+	
+	if(two==alter4 || two==alter5)
+	{
+		alert("Right Answer");
+	}
+	else
+	{
+		alert("Wrong Answer");
+	}
+	
+	
+	function func2()
+	{
+		document.write("<h1>Correct Answer</h1>");
+		document.write("<br>")
+		document.write("some students like to study in the night");
+		document.write("<br>");
+		document.write("at night some students like to study"); 
+	}
+	
+	sleep(4000);
+	
+	var three="John and Mary went to church";
+	var threes=three.split(' ');
+	document.write(threes);
+	
+	var alter6=["John and Mary went to church"];
+	var alter7=["Mary and John went to church"];
+   
+	
+	if(three==alter6 || three==alter7)
+	{
+		alert("Right Answer");
+	}
+	else
+	{
+		alert("Wrong Answer");
+	}
+	
+	function func3()
+	{
+		document.write("<h1>Correct Answer</h1>");
+		document.write("<br>")
+		document.write("John and Mary went to church");
+		document.write("<br>");
+		document.write("Mary and John went to church"); 
+		document.write("<br>");
+	}
+	
+	sleep(4000);
+	
+	var four="John went to church after eating    ";
+	var fours=four.split(' ');
+	document.write(fours);
+	
+	var alter8=["John went to church after eating"];
+	var alter9=["after eating John went to church"];
+	var alter10=[ "John after eating went to church"];
+   
+	
+	if(four==alter8 || four==alter9 || four==alter10)
+	{
+		alert("Right Answer");
+	}
+	else
+	{
+		alert("Wrong Answer");
+	}
+	
+	function func4()
+	{
+		document.write("<h1>Correct Answer</h1>");
+		document.write("<br>")
+		document.write("John went to church after eating");
+		document.write("<br>");
+		document.write("after eating John went to church"); 
+		document.write("<br>");
+		document.write("John after eating went to church");
+	}
+	
+	sleep(4000);
+	
+	var five="did he go to market";
+	var fives=five.split(' ');
+	document.write(fives);
+	
+	var alter11=["did he go to market"];
+	var alter12=["he did go to market"];
+   
+	
+	if(five==alter11 || five==alter12)
+	{
+		alert("Right Answer");
+	}
+	else
+	{
+		alert("Wrong Answer");
+	}
+	
+	function func5()
+	{
+		document.write("<h1>Correct Answer</h1>");
+		document.write("<br>")
+		document.write("did he go to market");
+		document.write("<br>");
+		document.write("he did go to market"); 
+		document.write("<br>");
+	}
+	
+	sleep(4000);
+	
+	var six="the woman who called my sister sells cosmetics";
+	var sixs=six.split(' ');
+	document.write(sixs);
+	
+	var alter13=["the woman who called my sister sells cosmetics"];
+	var alter14=["the woman who sells cosmetics called my sister"];
+	var alter15=["my sister who sells cosmetics called the woman"];
+	var alter16=["my sister who called the woman sells cosmetics"];
+   
+	
+	if(six==alter13 || six==alter14 || six==alter15 || six==alter16)
+	{
+		alert("Right Answer");
+	}
+	else
+	{
+		alert("Wrong Answer");
+	}
+	
+	function func6()
+	{
+		document.write("<h1>Correct Answer</h1>");
+		document.write("<br>")
+		document.write("the woman who called my sister sells cosmetics");
+		document.write("<br>");
+		document.write("the woman who sells cosmetics called my sister"); 
+		document.write("<br>");
+		document.write("my sister who sells cosmetics called the woman");
+		document.write("<br>");
+		document.write("my sister who called the woman sells cosmetics");
+		document.write("<br>");
+	}
+	
+	sleep(4000);
+	
+	var seven="John goes to the library and studies";
+	var sevens=seven.split(' ');
+	document.write(sevens);
+	
+	var alter17=["John goes to the library and studies"];
+	var alter18=["John studies and goes to the library"];
+   
+	
+	if(seven==alter17 || seven==alter18)
+	{
+		alert("Right Answer");
+	}
+	else
+	{
+		alert("Wrong Answer");
+	}
+	
+	function func7()
+	{
+		document.write("<h1>Correct Answer</h1>");
+		document.write("<br>")
+		document.write("John goes to the library and studies");
+		document.write("<br>");
+		document.write("John studies and goes to the library"); 
+		document.write("<br>");
+	}
+	
+	sleep(4000);
+	
+	var eight="John ate an apple so did she";
+	var eights=eight.split(' ');
+	document.write(eights);
+	
+	var alter19=["John ate an apple so did she"];
+	var alter20=["she ate an apple so did John"];
+
+	
+	if(eight==alter19 || eight==alter20)
+	{
+		alert("Right Answer");
+	}
+	else
+	{
+		alert("Wrong Answer");
+	}
+	
+	
+	function func8()
+	{
+		document.write("<h1>Correct Answer</h1>");
+		document.write("<br>")
+		document.write("John ate an apple so did she");
+		document.write("<br>");
+		document.write("she ate an apple so did John"); 
+		document.write("<br>");
+	}
+	
+	sleep(4000);
+	
+	var nine="the teacher returned the book after she noticed the error";
+	var nines=nine.split(' ');
+	document.write(nines);
+	
+	var alter21=["the teacher returned the book after she noticed the error"];
+	var alter22=["the teacher noticed the error after she returned the book"];
+	var alter23=["after the teacher returned the book she noticed the error"];
+    var alter24=["after the teacher noticed the error she returned the book"];
+	var alter25=["she returned the book after the teacher noticed the error"];
+	var alter26=["she noticed the error after the teacher returned the book"];
+	var alter27=["after she returned the book the teacher noticed the error"];
+	var alter28=["after she noticed the error the teacher returned the book"];
+   
+   
+	
+	if(nine==alter21 || nine==alter22 || nine==alter23 || nine==alter24 || nine==alter25 || nine==alter26 || nine==alter27 || nine==alter28)
+	{
+		alert("Right Answer");
+	}
+	else
+	{
+		alert("Wrong Answer");
+	}
+	
+	function func9()
+	{
+		document.write("<h1>Correct Answer</h1>");
+		document.write("<br>")
+		document.write("the teacher returned the book after she noticed the error");
+		document.write("<br>");
+		document.write("the teacher noticed the error after she returned the book"); 
+		document.write("<br>");
+		document.write("after the teacher returned the book she noticed the error");
+		document.write("<br>");
+		document.write("after the teacher noticed the error she returned the book");
+		document.write("<br>");
+		document.write("she returned the book after the teacher noticed the error");
+		document.write("<br>");
+		document.write("she noticed the error after the teacher returned the book");
+		document.write("<br>");
+		document.write("after she returned the book the teacher noticed the error");
+		document.write("<br>");
+		document.write("after she noticed the error the teacher returned the book");
+	}
+	
+	sleep(4000);
+	
+	var ten="I told her that I bought a book yesterday";
+	var tens=ten.split(' ');
+	document.write(tens);
+	
+	var alter29=["I told her that I bought a book yesterday"];
+	var alter30=["I told her yesterday that I bought a book"];
+	var alter31=["yesterday I told her that I bought a book"];
+	var alter32=["I bought a book that I told her yesterday"];
+	var alter33=["I bought a book yesterday that I told her"];
+	var alter34=["yesterday I bought a book that I told her"];
+	
+	if(ten==alter29 || ten==alter30 || ten==alter31 || ten==alter32 || ten==alter33 || ten==alter34)
+	{
+		alert("Right Answer");
+	}
+	else
+	{
+		alert("Wrong Answer");
+	}
+	
+	function func10()
+	{
+		document.write("<h1>Correct Answer</h1>");
+		document.write("<br>")
+		document.write("I told her that I bought a book yesterday");
+		document.write("<br>");
+		document.write("I told her yesterday that I bought a book"); 
+		document.write("<br>");
+		document.write("yesterday I told her that I bought a book");
+		document.write("<br>");
+		document.write("I bought a book that I told her yesterday");
+		document.write("<br>");
+		document.write("I bought a book yesterday that I told her");
+		document.write("<br>");
+		document.write("yesterday I bought a book that I told her");
+	}
+	
+	
---
+++ RDIP/Codes/hindi.js
@@ -0,0 +1,299 @@
+	var ele="राम और श्याम बाजार गयें ";
+	var eles=ele.split(' ');
+	document.write(eles);
+	
+	var alter35=["राम और श्याम बाजार गयें"];
+	var alter36=["राम और श्याम गयें बाजार"];
+	var alter37=["बाजार गयें राम और श्याम"];
+	var alter38=["गयें बाजार राम और श्याम"]
+   
+	
+	if(ele==alter35 || ele==alter36 || ele==alter37 || ele==alter38)
+	{
+		alert("Right Answer");
+	}
+	else
+	{
+		alert("Wrong Answer");
+	}
+	
+	function func11()
+	{
+		document.write("<h1>Correct Answer</h1>");
+		document.write("<br>")
+		document.write("राम और श्याम बाजार गयें");
+		document.write("<br>");
+		document.write("राम और श्याम गयें बाजार"); 
+		document.write("<br>");
+		document.write("बाजार गयें राम और श्याम");
+		document.write("<br>");
+		document.write("गयें बाजार राम और श्याम");
+	}
+	
+	sleep(4000);
+	
+
+	var twe="राम सोया और श्याम भी ";
+	var twes=twe.split(' ');
+	document.write(twes);
+	
+	var alter39=["राम सोया और श्याम भी"];
+	var alter40=["श्याम सोया और राम भी"];
+	var alter41=["सोया श्याम और राम भी"];
+	var alter42=["सोया राम और श्याम भी"]
+   
+	
+	if(twe==alter39 || twe==alter40 || twe==alter41 || twe==alter42)
+	{
+		alert("Right Answer");
+	}
+	else
+	{
+		alert("Wrong Answer");
+	}
+	
+	function func12()
+	{
+		document.write("<h1>Correct Answer</h1>");
+		document.write("<br>")
+		document.write("राम सोया और श्याम भी");
+		document.write("<br>");
+		document.write("श्याम सोया और राम भी"); 
+		document.write("<br>");
+		document.write("सोया श्याम और राम भी");
+		document.write("<br>");
+		document.write("सोया राम और श्याम भी");
+	}
+	
+	sleep(4000);
+	
+	var thir="मैंने उसे बताया कि राम सो रहा है ";
+	var thirs=thir.split(' ');
+	document.write(thirs);
+	
+
+		var alter43=["मैंने उसे बताया कि राम सो रहा है"];
+		var alter44=["मैंने उसे बताया कि सो रहा है राम"];
+		var alter45=["उसे मैंने बताया कि राम सो रहा है"];
+		var alter46=["उसे मैंने बताया कि सो रहा है राम"];
+		var alter47=["मैंने बताया उसे कि राम सो रहा है"];
+		var alter48=["मैंने बताया उसे कि सो रहा है राम"];
+		var alter49=["उसे बताया मैंने कि राम सो रहा है"];
+		var alter50=["उसे बताया मैंने कि सो रहा है राम"];
+		var alter51=["बताया मैंने उसे कि राम सो रहा है"];
+		var alter52=["बताया मैंने उसे कि सो रहा है राम"];
+		var alter53=["बताया उसे मैंने कि राम सो रहा है"];
+		var alter54=["बताया उसे मैंने कि सो रहा है राम"];
+
+
+	
+   
+	
+	if(twe==alter43 || twe==alter44 || twe==alter45 || twe==alter46 || twe==alter47 || twe==alter48 || twe==alter49 || twe==alter50 || twe==alter51 || twe==alter52 || twe==alter53 || twe==alter54)
+	{
+		alert("Right Answer");
+	}
+	else
+	{
+		alert("Wrong Answer");
+	}
+	
+	function func13()
+	{
+		document.write("<h1>Correct Answer</h1>");
+		document.write("<br>")
+		document.write("मैंने उसे बताया कि राम सो रहा है"");
+		document.write("<br>");
+		document.write("मैंने उसे बताया कि सो रहा है राम"); 
+		document.write("<br>");
+		document.write("उसे मैंने बताया कि राम सो रहा है");
+		document.write("<br>");
+		document.write("उसे मैंने बताया कि सो रहा है राम");
+		document.write("<br>")
+		document.write("मैंने बताया उसे कि राम सो रहा है");
+		document.write("<br>");
+		document.write("मैंने बताया उसे कि सो रहा है राम"); 
+		document.write("<br>");
+		document.write("उसे बताया मैंने कि राम सो रहा है");
+		document.write("<br>");
+		document.write("उसे बताया मैंने कि सो रहा है राम");
+		document.write("<br>")
+		document.write("बताया मैंने उसे कि राम सो रहा है");
+		document.write("<br>");
+		document.write("बताया मैंने उसे कि सो रहा है राम"); 
+		document.write("<br>");
+		document.write("बताया उसे मैंने कि राम सो रहा है");
+		document.write("<br>");
+		document.write("बताया उसे मैंने कि सो रहा है राम");
+	}
+	
+	sleep(4000);
+	
+	var forti="राम खाकर सोया";
+	var fortis=forti.split(' ');
+	document.write(fortis);
+	
+	var alter55=["राम खाकर सोया"];
+	var alter56=["खाकर राम सोया"];
+	var alter57=["राम सोया खाकर"];
+	var alter58=["खाकर सोया राम"];
+	var alter59=["सोया राम खाकर"];
+	var alter60=["सोया खाकर राम"];
+	
+	if(forti==alter55 || forti==alter56 || forti==alter57 || forti==alter58 || forti==alter59 || forti==alter60)
+	{
+		alert("Right Answer");
+	}
+	else
+	{
+		alert("Wrong Answer");
+	}
+	
+	function func14()
+	{
+		document.write("<h1>Correct Answer</h1>");
+		document.write("<br>")
+		document.write("राम खाकर सोया");
+		document.write("<br>");
+		document.write("खाकर राम सोया"); 
+		document.write("<br>");
+		document.write("राम सोया खाकर");
+		document.write("<br>");
+		document.write("खाकर सोया राम");
+		document.write("<br>");
+		document.write("सोया राम खाकर"");
+		document.write("<br>");
+		document.write("सोया खाकर राम");
+	}
+	
+	sleep(4000);
+	
+	
+	var fift="राम और श्याम बाजार गयें ";
+	var fifts=fift.split(' ');
+	document.write(fifts);
+	
+	var alter61=["बिल्लियों को मारकर कुत्ता सो गया "];  
+	var alter62=["मारकर बिल्लियों को कुत्ता सो गया"];	
+	var alter63=["बिल्लियों को मारकर सो गया कुत्ता"];    
+	var alter64=["मारकर बिल्लियों को सो गया कुत्ता"];    
+	var alter65=["कुत्ता सो गया बिल्लियों को मारकर"];    
+	var alter66=["कुत्ता सो गया मारकर बिल्लियों को"];    
+	var alter67=["सो गया कुत्ता बिल्लियों को मारकर"];
+	var alter68=["सो गया कुत्ता मारकर बिल्लियों को"];
+   
+	
+	if(fift==alter61 || fift==alter62 || fift==alter63 || fift==alter64 || fift==alter65 || fift==alter66 || fift==alter67 || fift==alter68)
+	{
+		alert("Right Answer");
+	}
+	else
+	{
+		alert("Wrong Answer");
+	}
+	
+	function func15()
+	{
+		document.write("<h1>Correct Answer</h1>");
+		document.write("<br>")
+		document.write("बिल्लियों को मारकर कुत्ता सो गया");
+		document.write("<br>");
+		document.write("मारकर बिल्लियों को कुत्ता सो गया"); 
+		document.write("<br>");
+		document.write("बिल्लियों को मारकर सो गया कुत्ता");
+		document.write("<br>");
+		document.write("मारकर बिल्लियों को सो गया कुत्ता");
+		document.write("<br>")
+		document.write("कुत्ता सो गया बिल्लियों को मारकर");
+		document.write("<br>");
+		document.write("कुत्ता सो गया मारकर बिल्लियों को"); 
+		document.write("<br>");
+		document.write("सो गया कुत्ता बिल्लियों को मारकर");
+		document.write("<br>");
+		document.write("सो गया कुत्ता मारकर बिल्लियों को");
+	}
+	
+	sleep(4000);
+	
+	
+	var sixt="एक लाल किताब वहाँ है ";
+	var sixts=sixt.split(' ');
+	document.write(sixts));
+	
+	var alter69=["एक लाल किताब वहाँ है"];
+	var alter70=["एक लाल किताब है वहाँ"];
+	var alter71=["वहाँ है एक लाल किताब"];
+	var alter72=["है वहाँ एक लाल किताब"];
+   
+	
+	if(sixt==alter69 || sixt==alter70 || sixt==alter71 || sixt==alter72)
+	{
+		alert("Right Answer");
+	}
+	else
+	{
+		alert("Wrong Answer");
+	}
+	
+	function func16()
+	{
+		document.write("<h1>Correct Answer</h1>");
+		document.write("<br>")
+		document.write("एक लाल किताब वहाँ है");
+		document.write("<br>");
+		document.write("एक लाल किताब है वहाँ"); 
+		document.write("<br>");
+		document.write("वहाँ है एक लाल किताब");
+		document.write("<br>");
+		document.write("है वहाँ एक लाल किताब");
+	}
+	
+	sleep(4000);
+	
+	var sevent="राम और श्याम बाजार गयें ";
+	var sevents=sevent.split(' ');
+	document.write(sevents);
+	
+	
+	var alter73=["एक बड़ी सी किताब वहाँ है "];   
+	var alter74=["एक बड़ी सी किताब है वहाँ"];
+	var alter75=["बड़ी सी एक किताब वहाँ है"];
+	var alter76=[" बड़ी सी एक किताब है वहाँ"];
+	var alter77=["वहाँ है एक बड़ी सी किताब"];
+	var alter78=["वहाँ है बड़ी सी एक किताब"];
+	var alter79=["है वहाँ एक बड़ी सी किताब"];
+	var alter80=["है वहाँ बड़ी सी एक किताब"];
+
+   
+	
+	if(sevent==alter73 || sevent==alter74 || sevent==alter75 || sevent==alter76 || sevent==alter77 || sevent==alter78 || sevent==alter79 || sevent==alter80)
+	{
+		alert("Right Answer");
+	}
+	else
+	{
+		alert("Wrong Answer");
+	}
+	
+	function func17()
+	{
+		document.write("<h1>Correct Answer</h1>");
+		document.write("<br>")
+		document.write("एक बड़ी सी किताब वहाँ है ");
+		document.write("<br>");
+		document.write("एक बड़ी सी किताब है वहाँ"); 
+		document.write("<br>");
+		document.write("बड़ी सी एक किताब वहाँ है");
+		document.write("<br>");
+		document.write("बड़ी सी एक किताब है वहाँ");
+		document.write("<br>")
+		document.write("वहाँ है एक बड़ी सी किताब");
+		document.write("<br>");
+		document.write("वहाँ है बड़ी सी एक किताब"); 
+		document.write("<br>");
+		document.write("है वहाँ एक बड़ी सी किताब");
+		document.write("<br>");
+		document.write("है वहाँ बड़ी सी एक किताब");
+	}
+	
+	sleep(4000);
